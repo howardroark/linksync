b2e58a3aab7ecd3bd9dc5fd87c7e4079dc6c1a82
howardroark <howardroark@users.noreply.github.com>
2017-06-03 12:31:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask
 from flask_sockets import Sockets
 import os
 
-
 app = Flask(__name__)
 sockets = Sockets(app)
-
 
 @sockets.route('/<hash>')
 def echo_socket(ws):
@@ -13,14 +11,6 @@ def echo_socket(ws):
         message = ws.receive()
         ws.send(message)
 
-
 @app.route('/')
 def index():
     return 'linksync'
-
-
-if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', int(os.environ.get('PORT', 5000))), app, handler_class=WebSocketHandler)
-    server.serve_forever()
