ce8b7f5fd38f01b787f5fcd2126b40076c6c9b3e
howardroark <howardroark@users.noreply.github.com>
2017-06-04 09:39:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var path = require('path');
 var io = require('socket.io');
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.listen(process.env.PORT);
 
 io.sockets.on('connection', function (socket) {
